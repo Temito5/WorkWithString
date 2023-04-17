5cc902d312aa8e982dca2ef419d1cdb7a39028b8
Staiko <Staiko@DESKTOP-NUVQ8TO>
2023-04-17 13:40:32 +0300

    Add project files.

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string shablon = Console.ReadLine();
+            string output = input;
+            byte couter = 0;
+
+            while (output.Contains(shablon))
+            {
+                couter++;
+                int pos = output.IndexOf(shablon);
+                int len = shablon.Length;
+                output = output.Remove(pos, len);
+            }
+
+            if (couter >= 2)
+            {
+                Console.WriteLine("Shaked it!");
+            }
+            Console.WriteLine("No shake!");
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Zadacha4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] word = Console.ReadLine().Split().ToArray();
+            string izrechenie = Console.ReadLine();
+
+            foreach (var item in word)
+            {
+                if (izrechenie.Contains(item))
+                {
+                    izrechenie = izrechenie.Replace(item, new string('*', item.Length));
+                }
+            }
+            Console.WriteLine(izrechenie);
+        }
+    }
+}
